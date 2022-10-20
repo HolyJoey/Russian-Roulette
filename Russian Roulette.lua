00a5a63d2c3198a40377233eb692eba93e7e554b
Holy <74293660+HolyJoey@users.noreply.github.com>
2022-10-20 22:57:01 +0200

    Add files via upload

---
+++ Russian Roulette.lua
@@ -0,0 +1,20 @@
+--Made by Holy#9756
+util.require_natives(1660775568)
+
+randomizer = function(x)
+    local r = math.random(1,3)
+    return x[r]
+end
+
+array = {"1","1","2"}
+
+menu.action(menu.my_root(), "Pull the trigger", { "RussianRoulette" }, "Play russian roulette with your game", function()
+    if randomizer(array) == "1" then
+        util.toast("Your game survived")
+    else
+        util.log("Looks like your game died")
+        ENTITY.APPLY_FORCE_TO_ENTITY(0, 0, 0, 0, 0, 0, 0, 0, 0, false, false, false, false, false)
+    end
+end)
+
+util.keep_running()
