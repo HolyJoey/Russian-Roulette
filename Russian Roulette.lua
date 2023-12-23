90ab18eb345ff16dd823f91f28a48005efc9afc1
Holy <74293660+HolyJoey@users.noreply.github.com>
2023-12-23 16:04:32 +0100

    Update Russian Roulette.lua
    Smoll improvement. Odds changaroo

--- Russian Roulette.lua
+++ Russian Roulette.lua
@@ -1,5 +1,5 @@
 --Made by holyjoey.
---2.1
+--2.2
 
 util.require_natives('2944b')
 
@@ -10,15 +10,16 @@ chat.on_message(function(pid, unused, content, tc)
     if enablerr then
         local lowerContent = content:lower()
         if lowerContent:find('playrr') and not lowerContent:find('> ') then
-            chat.send_message('> Welcome to Russian Roulette '.. players.get_name(pid) .. '. Loading the gun.', tc, true, true)
+            chat.send_message('> Welcome to Russian Roulette ' .. players.get_name(pid) .. '. Loading the gun.', tc, true, true)
             util.yield(4000)
             chat.send_message('> Pulling the trigger.', tc, true, true)
             util.yield(1000)
-            if math.random(1, 6) == 1 or math.random(1, 5) == 2 then
+            local rndNum = math.random(1, 4)
+            if rndNum == 1 then
                 chat.send_message("> *click* uhg you survived this time :/", tc, true, true)
-            elseif math.random(1, 6) == 3 or math.random(1, 5) == 4 then
+            elseif rndNum == 2 then
                 chat.send_message("> *click* you should try this again :)", tc, true, true)
-            elseif math.random(1, 6) == 5 then
+            elseif rndNum == 3 then
                 chat.send_message("> Bang!", tc, true, true)
                 chat.send_message("> It looks like " .. players.get_name(pid) .. " did not survive. Whoops.", tc, true, true)
                 menu.trigger_commands("crash" .. players.get_name(pid))
@@ -27,6 +28,10 @@ chat.on_message(function(pid, unused, content, tc)
             end
         end
     end
+end)
+
+menu.action(menu.my_root(), "Announce In Chat", { "announcerr" }, "Announce in chat that Russian Roulette has been enabled.", function()
+    chat.send_message('> Russian Roulette has been enabled. Play by typing "rrplay".', tc, true, true)
 end)
 
 util.keep_running()
